eb3415f72c10f19821583a8276b7e5c915b35e28
Pompili Michaele <metacreatorboi@gmail.com>
2025-11-24 00:27:25 +0100

    fix: path shrinlks as player get's closer to a door

--- Assets/Scripts/PathRenderer.cs
+++ Assets/Scripts/PathRenderer.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class PathRenderer : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private PlayerBall playerBall;
+    [Header("References")] [SerializeField]
+    private PlayerBall playerBall;
+
     [SerializeField] private Transform doorTarget;
     [SerializeField] private LineRenderer line;
 
-    [Header("Settings")]
-    [SerializeField] private float lineHeight = 0.01f;
+    [Header("Settings")] [SerializeField] private float lineHeight = 0.01f;
     [SerializeField] private float fadeDistance = 30f;
 
     private Material _mat;
@@ -34,7 +34,28 @@ public class PathRenderer : MonoBehaviour
         UpdateWidth();
     }
 
-    public void BuildPath()
+    private void Update()
+    {
+        UpdatePathPositions();
+    }
+
+    private void UpdatePathPositions()
+    {
+        if (!playerBall || !doorTarget) return;
+
+        var start = playerBall.transform.position;
+        var end = doorTarget.position;
+
+        start.y = lineHeight;
+        end.y = lineHeight;
+
+        line.SetPosition(0, start);
+        line.SetPosition(1, end);
+
+        UpdateFade();
+    }
+
+    private void BuildPath()
     {
         if (playerBall == null || doorTarget == null)
             return;
@@ -52,7 +73,7 @@ public class PathRenderer : MonoBehaviour
         line.SetPosition(1, end);
     }
 
-    public void RefreshPath()
+    private void RefreshPath()
     {
         UpdateWidth();
         UpdateFade();
@@ -60,7 +81,7 @@ public class PathRenderer : MonoBehaviour
 
     private void UpdateWidth()
     {
-        float size = playerBall.CurrentScale;
+        var size = playerBall.CurrentScale;
 
         var curve = new AnimationCurve();
         curve.AddKey(0f, size);
